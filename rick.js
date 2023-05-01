eeb97a04103485b16cfe23b0507fca904cfbb573
lucasrios21 <lucasquintero.iearm@gmail.com>
2023-05-01 15:13:14 -0500

    first commit

---
+++ rick.js
@@ -0,0 +1,26 @@
+const apiRick = async (pagina) => {
+    let url = "https://rickandmortyapi.com/api/character/?page=" + pagina;
+    const api = await fetch(url);
+    const data = await api.json();
+    console.log(data);
+    divRes=document.querySelector("#resultado");
+    divRes.innerHTML=""
+    data.results.map(item => {
+        divItem = document.createElement('div')
+        divItem.innerHTML= `
+        <div class="card" style="width: 18rem;">
+        <img src="${item.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Nombre: ${item.name}</h5>
+            <p class="card-text">Estado: ${item.status}</p>
+            <p class="card-text">Especie: ${item.especies}</p>
+            <p class="card-text">Género: ${item.gender}</p>
+            <p class="card-text">Locación: ${item.location.name}</p>
+            
+        </div>
+        </div>
+        `
+        divRes.appendChild(divItem);
+    })
+}
+apiRick(1);
